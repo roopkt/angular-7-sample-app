1506a6dea60c9ba567b822e6d6ce3e7b1aac5023
rtiwari <rtiwari@strasz.com>
2019-04-03 17:43:03 -0400

    hosted my app locally using expressjs

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const  express  = require('express');
+const port = process.env.PORT || 3000;
+const publicWeb = process.env.PUBLICWEB || './dist/pm-pinch';
+
+const app = express();
+
+app.use(express.static(publicWeb));
+console.log(`serving from ${publicWeb}`);
+
+app.get('*', (req, res) => {
+  res.sendFile(`index.html`, { root: publicWeb });
+});
+
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
